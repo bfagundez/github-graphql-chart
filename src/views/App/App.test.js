fb952b5061b239443488e18c06c33efb135ca282
Bruno Fagundez <bfagundez@gmail.com>
2018-02-22 21:00:19 -0800

    Added a title

--- src/views/App/App.test.js
+++ src/views/App/App.test.js
@@ -4,8 +4,14 @@ import { shallow } from 'enzyme'
 import App from './App'
 
 describe('<App />', () => {
+
   it('renders the app correctly', () => {
     const wrapper = shallow(<App />)
     expect(wrapper.find(App)).to.exist
+  })
+
+  it('renders the app title', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.contains(<p> Volume of Files Changed by Date </p>)).to.equal(true)
   })
 })

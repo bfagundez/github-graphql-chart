2eb4629384dfdfa1f9a08a0d7c6604e86068d83f
Bruno Fagundez <bfagundez@gmail.com>
2018-02-22 23:07:07 -0800

    Added actions, polished the reducer. Added some tests, the logic that formats the data should be in a selector

--- src/reducers/gitDataReducer.js
+++ src/reducers/gitDataReducer.js
@@ -1,17 +1,39 @@
+import { FETCH_REPO_DATA_PENDING,
+          FETCH_REPO_DATA_REJECTED,
+          FETCH_REPO_DATA_FULFILLED
+        } from 'constants/actionTypes'
+
 import _ from 'lodash'
 
 const initialState = {}
 
+function reshapeGitData (rawGraphQLData) {
+  let githubData = rawGraphQLData
+  let repos = _.get(githubData, 'data.query.repositories.edges', [])
+  let chartData = {label: 'Activity', values: []}
+
+  for (var i = 0; i < repos.length; i++) {
+    let tempRepoData = _.get(repos[i], 'node.ref.target.history.edges', [])
+    for (var c = 0; c < tempRepoData.length; c++) {
+      chartData.values.push({
+        y: _.get(tempRepoData[c], 'node.changedFiles', 0),
+        x: new Date(_.get(tempRepoData[c], 'node.committedDate', null))
+      })
+    }
+  }
+  return chartData
+}
+
 export default function gitData (state = initialState, action) {
   switch (action.type) {
-    case 'FETCH_REPO_DATA_PENDING': {
+    case FETCH_REPO_DATA_PENDING: {
       return {...state, fetching: true}
     }
-    case 'FETCH_REPO_DATA_REJECTED': {
+    case FETCH_REPO_DATA_REJECTED: {
       return { ...state, fetching: false, error: action.payload }
     }
-    case 'FETCH_REPO_DATA_FULFILLED': {
-      return { ...state, fetching: false, activity: action.payload.data }
+    case FETCH_REPO_DATA_FULFILLED: {
+      return { ...state, fetching: false, activity: reshapeGitData(action.payload.data) }
     }
     default:
       return state
---
+++ src/reducers/gitDataReducer.test.js
@@ -0,0 +1,119 @@
+import reducer from 'reducers/gitDataReducer'
+import * as types from 'constants/ActionTypes'
+
+describe('git Data reducer', () => {
+  it('should return the initial state', () => {
+    expect(reducer(undefined, {})).toEqual({})
+  })
+
+  it('should handle FETCH_REPO_DATA_PENDING', () => {
+    expect(
+      reducer({}, {
+        type: types.FETCH_REPO_DATA_PENDING
+      })
+    ).toEqual({
+        fetching: true
+    })
+  })
+
+  it('should handle FETCH_REPO_DATA_REJECTED', () => {
+    expect(
+      reducer({}, {
+        type: types.FETCH_REPO_DATA_REJECTED,
+        payload: 'Error found',
+        fetching: false
+      })
+    ).toEqual({
+        error: 'Error found',
+        fetching: false
+    })
+  })
+
+  it('should handle FETCH_REPO_DATA_FULFILLED', () => {
+    expect(
+      reducer({}, {
+        type: types.FETCH_REPO_DATA_FULFILLED,
+        payload: { "data": {
+  "data":{
+    "query":{
+      "avatarUrl":"https://avatars3.githubusercontent.com/u/559456?v=4",
+      "location":null,
+      "repositories":{
+        "edges":[
+          {
+            "node":{
+              "name":"repo-a",
+              "ref":{
+                "target":{
+                  "id":"MDY6Q29tbWl0NjA1NTc2Nzo0NDU5ZjM0ZjljZGI3N2U3OTQ2MWM4ZjM3ODJiMmQ2Yzg2YWZiYmU0",
+                  "history":{
+                    "pageInfo":{
+                      "hasNextPage":false
+                    },
+                    "edges":[
+                      {
+                        "node":{
+                          "committedDate":"2012-10-06T02:44:35Z",
+                          "changedFiles":9
+                        }
+                      },
+                      {
+                        "node":{
+                          "committedDate":"2012-10-05T04:19:53Z",
+                          "changedFiles":1
+                        }
+                      }
+                    ]
+                  }
+                }
+              }
+            }
+          },
+          {
+            "node":{
+              "name":"repo-b",
+              "ref":{
+                "target":{
+                  "id":"MDY6Q29tbWl0NjA1NTc2Nzo0NDU5ZjM0ZjljZGI3N2U3OTQ2MWM4ZjM3ODJiMmQ2Yzg2YWZiYmU0",
+                  "history":{
+                    "pageInfo":{
+                      "hasNextPage":false
+                    },
+                    "edges":[
+                      {
+                        "node":{
+                          "committedDate":"2012-10-06T02:44:35Z",
+                          "changedFiles":9
+                        }
+                      },
+                      {
+                        "node":{
+                          "committedDate":"2012-10-05T04:19:53Z",
+                          "changedFiles":1
+                        }
+                      }
+                    ]
+                  }
+                }
+              }
+            }
+          }
+
+        ]
+      }
+    }
+  }
+} },
+        fetching: false
+      })
+    ).toEqual({
+        fetching: false,
+        activity: { label: 'Activity',
+                    values:
+                     [ { y: 9, x: new Date("2012-10-06T02:44:35.000Z") },
+                       { y: 1, x: new Date("2012-10-05T04:19:53.000Z") },
+                       { y: 9, x: new Date("2012-10-06T02:44:35.000Z") },
+                       { y: 1, x: new Date("2012-10-05T04:19:53.000Z") } ] }
+    })
+  })
+})

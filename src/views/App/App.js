0bf616b053d686d8bc74c05584c7fe8b5170f5d8
Bruno Fagundez <bfagundez@gmail.com>
2018-02-22 23:08:11 -0800

    Added some store mocking to app tests, added some conditional logic

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,26 +1,53 @@
 import React, { Component } from 'react'
 import GraphQLApi from 'services/gitGraphQL'
 import ChartContainer from 'components/ChartContainer'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 import './app.scss'
 
-class App extends Component {
-  render () {
+export class App extends Component {
+  componentWillMount () {
+    this.store = this.props.store
+    this.onfetchGitData()
+  }
 
-    const activity = {
-      label: 'Number of commits and dates ',
-      values: [
-        {x: new Date(2015, 2, 5), y: 1},
-        {x: new Date(2015, 2, 6), y: 2},
-        {x: new Date(2015, 2, 7), y: 0}
-      ]}
+  onfetchGitData = (username = 'bfagundez') => {
+    const api = new GraphQLApi()
+    this.props.dispatch({
+      type: 'FETCH_REPO_DATA',
+      payload: api.query(username)
+    })
+  }
+
+  render () {
+    let scatterPlot = null
+    let loading = null
+
+    if (this.props.gitData.activity) {
+      scatterPlot = <ChartContainer data={this.props.gitData.activity} />
+    }
+
+    if (this.props.gitData.fetching) {
+      loading = <p>Loading... </p>
+      scatterPlot = null
+    }
 
     return (
       <div styleName='App'>
         <p> Volume of Files Changed by Date </p>
-        <ChartContainer data={activity} />
+        {loading}
+        {scatterPlot}
       </div>
     )
   }
 }
 
-export default App
+const mapStateToProps = (state) => {
+  return { gitData: state.gitData }
+}
+
+App.propTypes = {
+  gitData: PropTypes.object
+}
+
+export default connect(mapStateToProps)(App)
--- src/views/App/App.test.js
+++ src/views/App/App.test.js
@@ -1,22 +1,31 @@
+//import 'jsdom-global/register'
 import React from 'react'
 import { expect } from 'chai'
+import sinon from 'sinon'
 import { shallow } from 'enzyme'
 import ChartContainer from 'components/ChartContainer'
-import App from './App'
+import { App } from './App'
+import { createMockStore } from 'redux-test-utils'
+import {Provider} from 'react-redux';
 
 describe('<App />', () => {
   it('renders the app correctly', () => {
-    const wrapper = shallow(<App />)
+    const store = createMockStore({ fetching: true, gitHubData: {} })
+    const props = {}
+
+    const wrapper = shallow(
+      <Provider store={store}>
+        <App props={props}/>
+      </Provider>, {disableLifecycleMethods: true})
     expect(wrapper.find(App)).to.exist
   })
 
-  it('renders the app title', () => {
-    const wrapper = shallow(<App />)
-    expect(wrapper.contains(<p> Volume of Files Changed by Date </p>)).to.equal(true)
-  })
-
   it('renders the Chart container', () => {
-    const wrapper = shallow(<App />)
+    const store = createMockStore({ fetching: true, gitHubData: {} })
+    const props = {}
+    const wrapper = shallow(<Provider store={store}>
+      <App props={props}/>
+    </Provider>)
     expect(wrapper.find(ChartContainer)).to.exist
   })
 })
